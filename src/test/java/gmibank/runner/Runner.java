aff86389e282ddfed71b563a141f4de80f32175b
Emine <btrusa45@gmail.com>
2021-02-06 20:18:53 -0600

    US 25

--- src/test/java/gmibank/runner/Runner.java
+++ src/test/java/gmibank/runner/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 		},
 		features = "src/test/resources/features",
 		glue = "gmibank/step_defs",
-		tags = "@s5",
+		tags = "@US_025_API",
 		dryRun = false
 )
 public class Runner {
